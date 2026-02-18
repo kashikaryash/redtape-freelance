947bd0212a534d4a7b502d4f191ecbb8faa6de80
kashikaryash <kashikaryash@gmail.com>
2026-02-18 15:44:25 +0530

    ABC

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'loyal-benevolence-production.up.railway.app/API',
     devAutoLoginEmail: 'yash983496@gmail.com'
 };
